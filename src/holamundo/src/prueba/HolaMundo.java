db4c56f0351eb6ccbdd940d0b6c1b3e8814d5917
Name <Email>
2017-04-16 17:48:06 -0300

    refactor

--- src/holamundo/src/prueba/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-package holamundo.src.prueba;
-
-public class HolaMundo {
-
-	public static void main(String[] args) {
-		System.out.println("Hola mundo en Java");
-	}
-
-}
---
+++ src/main/java/holamundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo en Java");
+	}
+
+}
