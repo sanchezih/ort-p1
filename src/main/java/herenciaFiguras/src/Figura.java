75ee207c0d0c80af5f74a9edf95c6ce5f67e54c1
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-03-31 16:47:48 -0300

    Update Figura.java

--- src/main/java/herenciaFiguras/src/Figura.java
+++ src/main/java/herenciaFiguras/src/Figura.java
@@ -1,23 +1,19 @@
 package herenciaFiguras.src;
 
-
 /**
  * Write a description of class Figura here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class Figura
-{
-    protected float area;
-    
-    public Figura()
-    {
-        area = 0;
-    }
-    
-    public float accedeArea()   
-    {
-        return area;
-    }
+public class Figura {
+	protected float area;
+
+	public Figura() {
+		area = 0;
+	}
+
+	public float accedeArea() {
+		return area;
+	}
 }
