61faee9fb8f6f79a61f771abf6912c55037ef6fb
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-04-04 16:04:32 -0300

    creo ejemplo instanceof

---
+++ src/main/java/ejInstanceof/src/ejemplo01/Main.java
@@ -0,0 +1,11 @@
+package ejInstanceof.src.ejemplo01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s = new String("No leas esto, sólo es un ejemplo");
+		if (s instanceof String)
+			System.out.println("Efectivamente s pertenece a la clase String");
+	}
+
+}
---
+++ src/main/java/ejInstanceof/src/ejemplo02/src/Perro.java
@@ -0,0 +1,24 @@
+package ejInstanceof.src.ejemplo02.src;
+
+class Animal {
+}
+
+class Perro extends Animal {
+	public static void main(String[] args) {
+		Perro toby = new Perro();
+		if (toby instanceof Animal)
+			System.out.println("toby es un perro y también un animal");
+
+		Animal chusa = new Perro(); /* creacion */
+		downCast(chusa);
+	}
+
+	public static void downCast(Animal a) {
+		if (a instanceof Perro)
+			((Perro) a).ladrar();
+	}
+
+	public static void ladrar() {
+		System.out.println("Guau");
+	}
+}
