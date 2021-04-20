f29331c26ab372a409dc9bfa973938c2ed6e938f
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-04-20 15:23:04 -0300

    ejercicio c del tp3 puerto barcos

--- src/main/java/tp3_puerto_barcos/src/Main.java
+++ src/main/java/tp3_puerto_barcos/src/Main.java
@@ -9,17 +9,17 @@ public class Main {
 	public static void main(String[] args) {
 
 		// creo los barcos
-		Barco yate1 = new Yate("ARG-5999", 17, 1980, "Julia Perez", 5);
-		Barco velero1 = new Velero("ARG-5830", 12, 1997, "Alejandro Ruiz", 3);
-		Barco deportivoComun1 = new DeportivoComun("ARG-5999", 17, 1980, "Julia Perez",  1500, 8);
+	//	Barco yate1 = new Yate("ARG-5999", 17, 1980, "Julia Perez", 5);
+	//	Barco velero1 = new Velero("ARG-5830", 12, 1997, "Alejandro Ruiz", 3);
+		Barco deportivoComun1 = new DeportivoComun("ARG-5999", 17, 1980, "Julia Perez", 1500, 8);
 
 		// creo las amarras
 		Amarra a1 = new Amarra(1, "norte", true);
 		Amarra a2 = new Amarra(2, "norte", true);
 
 		// asigno los barcos a las amarras
-		a1.setBarco(yate1);
-		a2.setBarco(velero1);
+		a1.setBarco(deportivoComun1);
+	//	a2.setBarco(velero1);
 
 		listaDeAmarras.add(a1);
 		listaDeAmarras.add(a2);
@@ -32,6 +32,8 @@ public class Main {
 				+ puertoParana.barcosConAlquilerMayorA(alquilerMinimo));
 
 		// Ejercicio C
+		System.out.println(
+				"El barco deportivo con mayor consumo de todo el puerto es: " + puertoParana.barcoConMayorConsumo());
 	}
 
 }
--- src/main/java/tp3_puerto_barcos/src/Puerto.java
+++ src/main/java/tp3_puerto_barcos/src/Puerto.java
@@ -3,8 +3,6 @@ package tp3_puerto_barcos.src;
 import java.util.ArrayList;
 
 public class Puerto {
-	private double valorBase = 10;
-
 	private ArrayList<Amarra> listaDeAmarras = new ArrayList<Amarra>();
 
 	public ArrayList<Amarra> getListaDeAmarras() {
@@ -20,10 +18,8 @@ public class Puerto {
 	}
 
 	public int barcosConAlquilerMayorA(double importeMinimo) {
-		System.out.println("Voy a calcular que cantidad de barcos que abonan un importe mayor a " + importeMinimo);
 		int cont = 0;
 		for (Amarra a : this.listaDeAmarras) {
-
 			if (a.getBarco() != null) {
 				System.out.println("El costo de alquiler del barco " + a.getBarco().getMatricula() + " es: "
 						+ a.getBarco().calcularCostoDelAlquiler());
@@ -37,17 +33,19 @@ public class Puerto {
 
 	public Deportivo barcoConMayorConsumo() {
 		Deportivo barcoConMayorConsumo = null;
-
 		for (Amarra a : this.listaDeAmarras) {
 			if (a.getBarco() != null) {
-				if (a.getBarco() instanceof Deportivo) {
-					if (((Deportivo) a.getBarco()).calcularConsumo() > barcoConMayorConsumo.calcularConsumo()) {
-						barcoConMayorConsumo = (Deportivo) a.getBarco();
+				if (barcoConMayorConsumo == null) {
+					barcoConMayorConsumo = (Deportivo) a.getBarco();
+				} else {
+					if (a.getBarco() instanceof Deportivo) {
+						if (((Deportivo) a.getBarco()).calcularConsumo() > barcoConMayorConsumo.calcularConsumo()) {
+							barcoConMayorConsumo = (Deportivo) a.getBarco();
+						}
 					}
 				}
 			}
 		}
 		return barcoConMayorConsumo;
-
 	}
 }
