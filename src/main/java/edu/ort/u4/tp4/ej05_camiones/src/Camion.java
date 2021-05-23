455dc4203f94af06d7310625518c4a3e0ab3b1ad
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-05-23 12:24:06 -0300

    agrego ejercicio camiones tp4

---
+++ src/main/java/edu/ort/u4/tp4/ej05_camiones/src/Camion.java
@@ -0,0 +1,29 @@
+package edu.ort.u4.tp4.ej05_camiones.src;
+
+public class Camion {
+	private int ID;
+	private double costoXKm;
+
+	public Camion(int iD, double costoXKm) {
+		super();
+		ID = iD;
+		this.costoXKm = costoXKm;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public double getCostoXKm() {
+		return costoXKm;
+	}
+
+	public void setCostoXKm(double costoXKm) {
+		this.costoXKm = costoXKm;
+	}
+
+}
