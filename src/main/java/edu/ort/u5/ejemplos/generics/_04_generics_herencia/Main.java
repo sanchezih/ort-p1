34512517d2df236aa2c41af1661ee691425636bd
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-05-26 18:49:15 -0300

    Update Main.java

--- src/main/java/edu/ort/u5/ejemplos/generics/_04_generics_herencia/Main.java
+++ src/main/java/edu/ort/u5/ejemplos/generics/_04_generics_herencia/Main.java
@@ -5,11 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		/* SIN CLASES GENERICAS */
+		// Tener en cuenta que para Java conoce la relacion entre Empleado y Jefe
 		// Empleado Administrativa = new Empleado("Elena", 45, 1500);
 		// Jefe DirectoraComercial = new Jefe("Ana", 47, 3500);
 		// Empleado nuevoEmpleado = DirectoraComercial;
 
 		/* CON CLASES GENERICAS */
+		// Tener en cuenta que para Java no existe relacion entre Clase<Empleado> y
+		// Clase<Jefe>
+
 		MiGenerica<Empleado> Administrativa = new MiGenerica<Empleado>();
 		MiGenerica<Jefe> DirectoraComercial = new MiGenerica<Jefe>();
 		// MiGenerica<Empleado> nuevoEmpleado = DirectoraComercial;
