0828211c7cb972b37997b96a562b0d415a74a655
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-06-03 12:37:02 -0300

    actualizo ejercicios

--- src/main/java/edu/ort/p1/u5/ejemplos/pila/pila_generica_con_lista_enlazada/Nodo.java
+++ src/main/java/edu/ort/p1/u5/ejemplos/pila/pila_generica_con_lista_enlazada/Nodo.java
@@ -1,27 +1,27 @@
 package edu.ort.p1.u5.ejemplos.pila.pila_generica_con_lista_enlazada;
 
-public class Nodo<TipoElemento> {
-	private TipoElemento elemento;
-	private Nodo<TipoElemento> next;
+public class Nodo<T> {
+	private T elemento;
+	private Nodo<T> next;
 
-	public Nodo(TipoElemento elemento) {
+	public Nodo(T elemento) {
 		this.elemento = elemento;
 		this.next = null;
 	}
 
-	public TipoElemento getElemento() {
+	public T getElemento() {
 		return elemento;
 	}
 
-	public void setElemento(TipoElemento elemento) {
+	public void setElemento(T elemento) {
 		this.elemento = elemento;
 	}
 
-	public Nodo<TipoElemento> getNext() {
+	public Nodo<T> getNext() {
 		return next;
 	}
 
-	public void setNext(Nodo<TipoElemento> next) {
+	public void setNext(Nodo<T> next) {
 		this.next = next;
 	}
 
