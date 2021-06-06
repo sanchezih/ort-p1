4a7cf1013c51d3cda0ed4f436d66f70dda3e9aff
Ignacio Sanchez <sanchezih@gmail.com>
2021-06-05 21:49:36 -0300

    cambio modificador de acceso

--- src/main/java/edu/ort/p1/u5/practico05/ej02_pelotas_tenis/Main.java
+++ src/main/java/edu/ort/p1/u5/practico05/ej02_pelotas_tenis/Main.java
@@ -3,6 +3,7 @@ package edu.ort.p1.u5.practico05.ej02_pelotas_tenis;
 public class Main {
 
 	public static void main(String[] args) {
+
 		PelotaDeTenis pelota1 = new PelotaDeTenis();
 		PelotaDeTenis pelota2 = new PelotaDeTenis();
 		PelotaDeTenis pelota3 = new PelotaDeTenis();
@@ -13,9 +14,7 @@ public class Main {
 		tubo1.push(pelota2);
 		tubo1.push(pelota3);
 
-		ClubDeTenis club = new ClubDeTenis();
-
-		System.out.println(club.esTuboUsado(tubo1));
+		System.out.println(ClubDeTenis.esTuboUsado(tubo1));
 
 	}
 
