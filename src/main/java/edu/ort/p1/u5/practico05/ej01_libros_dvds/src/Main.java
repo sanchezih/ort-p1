7c14b9a928c176e7f910c90a0f761eee8e0fff55
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-06-12 00:11:23 -0300

    Update Main.java

--- src/main/java/edu/ort/p1/u5/practico05/ej01_libros_dvds/src/Main.java
+++ src/main/java/edu/ort/p1/u5/practico05/ej01_libros_dvds/src/Main.java
@@ -8,15 +8,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Pila<DVD> pilaDeDVDs = new PilaGenericaAL<DVD>();
-		pilaDeDVDs.push(new DVD("Verbatim", true));
-		pilaDeDVDs.push(new DVD("Sony", false));
-		pilaDeDVDs.push(new DVD("TDK", true));
-
 		Pila<Libro> pilaDeLibros = new PilaNodos<Libro>();
 		pilaDeLibros.push(new Libro("978-987-86-4331-1", 319));
 		pilaDeLibros.push(new Libro("978-987-3832-50-5", 285));
 		pilaDeLibros.push(new Libro("978-987-1949-68-7", 292));
+
+		Pila<DVD> pilaDeDVDs = new PilaGenericaAL<DVD>(); // Utilizo otra impl de pila
+		pilaDeDVDs.push(new DVD("Verbatim", true));
+		pilaDeDVDs.push(new DVD("Sony", false));
+		pilaDeDVDs.push(new DVD("TDK", true));
 
 		Empleado e = new Empleado("Juan");
 		System.out.println("La cantidad de DVDs rayados es: " + e.cantDVDsRayados(pilaDeDVDs));
