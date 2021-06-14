e3f8fb3acab364420a28bb8eaa8b862f215b3c48
Ignacio Sanchez <sanchezih@gmail.com>
2021-06-14 09:41:44 -0300

    Update Main.java

--- src/main/java/edu/ort/p1/u5/practico05/ej03_imprimir_documentos/src/Main.java
+++ src/main/java/edu/ort/p1/u5/practico05/ej03_imprimir_documentos/src/Main.java
@@ -3,13 +3,19 @@ package edu.ort.p1.u5.practico05.ej03_imprimir_documentos.src;
 public class Main {
 
 	public static void main(String[] args) {
-		Impresora impresora = new Impresora(true, 10);
+
 		Documento doc1 = new Documento("2021-05-18", 4);
 		Documento doc2 = new Documento("2021-05-18", 3);
 
+		/*----------------------------------------------------------------------------*/
+
+		Impresora impresora = new Impresora(true, 10);
+
 		impresora.agregarDocumento(doc1);
 		impresora.agregarDocumento(doc2);
+
 		impresora.imprimir(false);
+
 		System.out.println(impresora.getCantHojasDisp());
 
 	}
