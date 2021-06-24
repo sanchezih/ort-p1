cec06163be2be3ddd302a76be0060670d405ac98
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-06-23 22:51:11 -0300

    del

--- src/main/java/edu/ort/p1/extra/exa/_2021c1/_2p/tema2/leon/src/OrdenEnCola.java
+++
@@ -1,8 +0,0 @@
-package edu.ort.p1.extra.exa._2021c1._2p.tema2.leon.src;
-
-public class OrdenEnCola {
-	
-	+public OrdenEnCola(Orden,int)
-	+OrdenEnCola(Orden,Double)
-
-}
--- src/main/java/edu/ort/p1/extra/exa/_2021c1/_2p/tema2/leon/src/Priorizable.java
+++
@@ -1,6 +0,0 @@
-package edu.ort.p1.extra.exa._2021c1._2p.tema2.leon.src;
-
-public interface Priorizable {
-
-	public int getPrioridad();
-}
