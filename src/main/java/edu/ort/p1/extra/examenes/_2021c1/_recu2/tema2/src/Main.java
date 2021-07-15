eaf134e66fc84ba31044af3f258ac98f02b22790
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-07-15 17:18:12 -0300

    agrego recu2 - tema2

---
+++ src/main/java/edu/ort/p1/extra/examenes/_2021c1/_recu2/tema2/src/Main.java
@@ -0,0 +1,14 @@
+package edu.ort.p1.extra.examenes._2021c1._recu2.tema2.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*----------------------------------------------------------------------------*/
+
+		EmpresaLogistica empresaDeLogistica = new EmpresaLogistica();
+
+		empresaDeLogistica.cantViajesSinDestino("Pacheco");
+	}
+
+}
