04327fbceca59e7829bd22c3eff01b72042530f6
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-08-05 10:38:53 -0300

    Update Main.java

--- src/main/java/ar/edu/ort/p1/u3/ejemplos/operador_instanceof/ejemplo04/src/Main.java
+++ src/main/java/ar/edu/ort/p1/u3/ejemplos/operador_instanceof/ejemplo04/src/Main.java
@@ -4,17 +4,15 @@ package ar.edu.ort.p1.u3.ejemplos.operador_instanceof.ejemplo04.src;
 interface Animal {
 }
 
-class Dog implements Animal {
+class Perro implements Animal {
 }
 
 class Main {
 	public static void main(String[] args) {
 
-// create an object of the Dog class
-		Dog d1 = new Dog();
+		Perro perro = new Perro(); // Instancio la clase Perro
 
-// checks if the object of Dog
-// is also an instance of Animal
-		System.out.println(d1 instanceof Animal); // returns true
+		/* checks if the object of Dog is also an instance of Animal */
+		System.out.println(perro instanceof Animal); // returns true
 	}
 }
