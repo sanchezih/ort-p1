3b5e5a3e4a1b4b84f75ed56714ffcdcca390ae22
Ignacio Sanchez <ignacio.sanchez@comafi.com.ar>
2021-08-07 12:44:09 -0300

    ordeno enums

--- src/main/java/ar/edu/ort/p1/u1/ejemplos/enums/_03_metodos/src/Empresa.java
+++ src/main/java/ar/edu/ort/p1/u1/ejemplos/enums/_03_metodos/src/Empresa.java
@@ -1,32 +1,24 @@
 package ar.edu.ort.p1.u1.ejemplos.enums._03_metodos.src;
 
-
-
 import java.util.ArrayList;
 
-/**
- *
- * @author Charly Cimino Aprendé más Java en mi canal:
- * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
- * GitHub: https://github.com/CharlyCimino
- */
 public class Empresa {
 
-    private ArrayList<Persona> listaPersonas;
+	private ArrayList<Persona> listaPersonas;
 
-    public Empresa() {
-        this.listaPersonas = new ArrayList<>();
-    }
+	public Empresa() {
+		this.listaPersonas = new ArrayList<>();
+	}
 
-    public void agregarPersona(Persona p) {
-        this.listaPersonas.add(p);
-    }
+	public void agregarPersona(Persona p) {
+		this.listaPersonas.add(p);
+	}
 
-    public void mostrarPorNivel(NivelEstudio nivel) {
-        for (Persona p : listaPersonas) {
-            if (p.getNivelEstudio() == nivel) {
-                System.out.println(p);
-            }
-        }
-    }
+	public void mostrarPorNivel(NivelEstudio nivel) {
+		for (Persona p : listaPersonas) {
+			if (p.getNivelEstudio() == nivel) {
+				System.out.println(p);
+			}
+		}
+	}
 }
