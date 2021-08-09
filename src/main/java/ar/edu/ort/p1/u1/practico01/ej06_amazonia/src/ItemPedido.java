19bfccf9e0ed28d2ea3eedb03b225dbcfb41b4d1
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-08-09 17:29:40 -0300

    agrego ej amazonia

---
+++ src/main/java/ar/edu/ort/p1/u1/practico01/ej06_amazonia/src/ItemPedido.java
@@ -0,0 +1,27 @@
+package ar.edu.ort.p1.u1.practico01.ej06_amazonia.src;
+
+public class ItemPedido {
+
+	private int cant;
+	private Producto producto;
+
+	public ItemPedido(int cant, Producto producto) {
+		this.cant = cant;
+		this.producto = producto;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public boolean hayStockSuficiente() {
+		return this.cant <= this.producto.getCantEnStock();
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	public void descontarStock() {
+		this.producto.descontar(this.cant);
+	}
+
+}
