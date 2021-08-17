1fee36982649adabb681074e46d6764475b6484a
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-08-17 11:30:06 -0300

    agrego ejercicio droides

---
+++ src/main/java/ar/edu/ort/p1/u1/practico01/ej04_droides/src/Pieza.java
@@ -0,0 +1,10 @@
+package ar.edu.ort.p1.u1.practico01.ej04_droides.src;
+
+public class Pieza {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+}
---
+++ src/main/java/ar/edu/ort/p1/u1/practico01/ej04_droides/src/ResultadoReparacion.java
@@ -0,0 +1,5 @@
+package ar.edu.ort.p1.u1.practico01.ej04_droides.src;
+
+public enum ResultadoReparacion {
+	COMPLETAMENTE_OPERATIVO, REPARACION_PARCIAL, REPARACION_IMPOSIBLE
+}
