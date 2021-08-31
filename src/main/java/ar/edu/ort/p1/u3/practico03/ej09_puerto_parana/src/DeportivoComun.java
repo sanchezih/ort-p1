c9d98c8b44405110de5cd3b3d221a82aa7179b9c
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-08-31 17:48:55 -0300

    saco mala impl de puerto

--- src/main/java/ar/edu/ort/p1/u3/practico03/ej09_puerto_parana/src/DeportivoComun.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.ort.p1.u3.practico03.ej09_puerto_parana.src;
-
-public class DeportivoComun extends Deportivo {
-
-	public DeportivoComun(String matricula, double eslora, int anioFabricacion, String nombreDuenio, double potencia,
-			int factorAerodinamico) {
-		super(matricula, eslora, anioFabricacion, nombreDuenio, potencia, factorAerodinamico);
-	}
-}
