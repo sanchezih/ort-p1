188d6b65c787df01f0c8ca17fc80f68d4b80a266
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-09-11 19:46:01 -0300

    agrego impl puerto_parana

---
+++ src/main/java/ar/edu/ort/p1/u3/practico03/ej09_puerto_parana/src/Deportivo.java
@@ -0,0 +1,32 @@
+package ar.edu.ort.p1.u3.practico03.ej09_puerto_parana.src;
+
+public class Deportivo extends Embarcacion {
+
+	private final static double INDICE_CALC_POTENCIA = 0.35;
+
+	private int potenciaMotor;
+	private int factorAerodinamico;
+
+	public int getPotenciaMotor() {
+		return potenciaMotor;
+	}
+
+	@Override
+	public double precioAdicional() {
+		return getValorAdicional() * (this.potenciaMotor * 0.5);
+	}
+
+	public double getConsumo() {
+		return potenciaMotor / factorAerodinamico * INDICE_CALC_POTENCIA;
+
+	}
+
+	/**
+	 * Metodo estatico
+	 * 
+	 * @return
+	 */
+	public static double GET_INDICE_CALC_POTENCIA() {
+		return INDICE_CALC_POTENCIA;
+	}
+}
