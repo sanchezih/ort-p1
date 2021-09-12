ad23690d998e3344dc5728b36a55502af7968ee6
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-09-12 20:37:24 -0300

    agrego ej ortprop

---
+++ src/main/java/ar/edu/ort/p1/u3/practico03/ej14_ortprop/src/Cliente.java
@@ -0,0 +1,22 @@
+package ar.edu.ort.p1.u3.practico03.ej14_ortprop.src;
+
+public class Cliente implements Notificable {
+
+	private String apellido;
+	private String nombre;
+	private String telefono;
+	private String email;
+
+	public Cliente(String apellido, String nombre, String telefono, String email) {
+		this.apellido = apellido;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.email = email;
+	}
+
+	@Override
+	public void recibirNotificacion(String mensaje) {
+		System.out.println("He recibido un SMS en mi teléfono " + this.telefono + " con el mensaje: " + mensaje);
+	}
+
+}
---
+++ src/main/java/ar/edu/ort/p1/u3/practico03/ej14_ortprop/src/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.ort.p1.u3.practico03.ej14_ortprop.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Notificable inmobiliaria01 = new Inmobiliaria("Remax", "info@remax.com", "4");
+		Notificable inmobiliaria02 = new Inmobiliaria("Lepore", "info@lepore.com", "5");
+
+	}
+
+}
