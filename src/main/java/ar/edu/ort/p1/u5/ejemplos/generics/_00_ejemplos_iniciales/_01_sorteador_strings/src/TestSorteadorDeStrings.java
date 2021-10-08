8c0510e2a8a4ca2e9cbde730f285dee941b43d34
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-10-08 19:41:14 -0300

    Update TestSorteadorDeStrings.java

--- src/main/java/ar/edu/ort/p1/u5/ejemplos/generics/_00_ejemplos_iniciales/_01_sorteador_strings/src/TestSorteadorDeStrings.java
+++ src/main/java/ar/edu/ort/p1/u5/ejemplos/generics/_00_ejemplos_iniciales/_01_sorteador_strings/src/TestSorteadorDeStrings.java
@@ -3,22 +3,22 @@ package ar.edu.ort.p1.u5.ejemplos.generics._00_ejemplos_iniciales._01_sorteador_
 public class TestSorteadorDeStrings {
 
 	public static void main(String[] args) {
-		SorteadorDeStrings ss = new SorteadorDeStrings();
+		SorteadorDeStrings sorteador = new SorteadorDeStrings();
 		
-		System.out.println("El sorteador de Strings esta vacio? "+ss.isEmpty());
+		System.out.println("El sorteador de Strings esta vacio? "+sorteador.isEmpty());
 		
-		ss.add("Perro");
-		ss.add("Lechuza");
-		ss.add("Caballo");
-		ss.add("Gato");
+		sorteador.add("Perro");
+		sorteador.add("Lechuza");
+		sorteador.add("Caballo");
+		sorteador.add("Gato");
 		
-		System.out.println("El sorteador de Strings esta vacio? "+ss.isEmpty());
+		System.out.println("El sorteador de Strings esta vacio? "+sorteador.isEmpty());
 		
-		while(!ss.isEmpty()) {
-			System.out.println(ss.remove().toUpperCase());
+		while(!sorteador.isEmpty()) {
+			System.out.println(sorteador.remove().toUpperCase());
 		}
 		
-		System.out.println("El sorteador de Strings esta vacio? "+ss.isEmpty());
+		System.out.println("El sorteador de Strings esta vacio? "+sorteador.isEmpty());
 	}
 
 }
