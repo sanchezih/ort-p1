97150be297f52b31db6a01ad50ba417271c17629
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-10-08 17:40:54 -0300

    add ej baraja espaniola

---
+++ src/main/java/ar/edu/ort/p1/u4/practico04/ej02_baraja_espaniola/src/Baraja.java
@@ -0,0 +1,61 @@
+package ar.edu.ort.p1.u4.practico04.ej02_baraja_espaniola.src;
+
+import java.util.Random;
+
+public class Baraja {
+	private static final int CANT_CARTAS = 48;
+	private Carta[] cartas = new Carta[CANT_CARTAS];
+
+	public Baraja() {
+		inicializarBaraja();
+	}
+
+	public boolean estaOrdenada() {
+		boolean estaOrdenada = true;
+		int idxPalo = 0;
+		Palo[] palos;
+
+		palos = Palo.values();
+		while (idxPalo < palos.length && estaOrdenada) {
+			estaOrdenada = estaOrdenada(palos[idxPalo]);
+			idxPalo++;
+		}
+		return estaOrdenada;
+	}
+
+	private boolean estaOrdenada(Palo palo) {
+		boolean estaOrdenada = true;
+		int i = 0;
+		Carta c;
+
+		while (i < Carta.MAX_NUM && estaOrdenada) {
+			c = this.cartas[Carta.MAX_NUM * palo.ordinal() + i];
+			estaOrdenada = c.getNumero() == (i + 1) && c.esDe(palo);
+			i++;
+		}
+		return estaOrdenada;
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	private void inicializarBaraja() {
+		int i = 0;
+		for (Palo p : Palo.values()) {
+			for (int j = 0; j < 12; j++) {
+				cartas[i] = new Carta(j + 1, p);
+				i++;
+			}
+		}
+		mezclarBaraja();
+	}
+
+	private void mezclarBaraja() {
+		Random random = new Random();
+		for (int i = 0; i < cartas.length; i++) {
+			int randomIndexToSwap = random.nextInt(cartas.length);
+			Carta temp = cartas[randomIndexToSwap];
+			cartas[randomIndexToSwap] = cartas[i];
+			cartas[i] = temp;
+		}
+	}
+}
---
+++ src/main/java/ar/edu/ort/p1/u4/practico04/ej02_baraja_espaniola/src/Palo.java
@@ -0,0 +1,5 @@
+package ar.edu.ort.p1.u4.practico04.ej02_baraja_espaniola.src;
+
+public enum Palo {
+	ORO, BASTO, ESPADA, COPA;
+}
