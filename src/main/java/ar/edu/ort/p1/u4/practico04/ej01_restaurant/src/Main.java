5f53ecab9136a9aea42da32626641a408419389a
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-10-19 11:42:21 -0300

    refactor ejercicio restaurant

--- src/main/java/ar/edu/ort/p1/u4/practico04/ej01_restaurant/src/Main.java
+++ src/main/java/ar/edu/ort/p1/u4/practico04/ej01_restaurant/src/Main.java
@@ -1,20 +1,10 @@
 package ar.edu.ort.p1.u4.practico04.ej01_restaurant.src;
 
-import ar.edu.ort.p1.util.UtilArrays;
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		Restaurant restaurant = new Restaurant();
-
-		// Test ejercicio B
-		System.out.println("Muestro el monto total de pedidos que tomo cada cajero");
-		UtilArrays.mostrar(restaurant.montosPorCajero());
-
-		// Test ejercicio C
-		System.out.println("\nMuestro el pedido con mas riesgo de no ser entregado");
-		restaurant.mostrarPedidoConMasRiesgo();
+		
 	}
 
 }
