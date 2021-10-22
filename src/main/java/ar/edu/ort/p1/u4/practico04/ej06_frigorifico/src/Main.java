1673979e29b927f2277a63a75b86f9c7a300afbb
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-10-22 17:22:39 -0300

    update ej frigorifico

---
+++ src/main/java/ar/edu/ort/p1/u4/practico04/ej06_frigorifico/src/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.ort.p1.u4.practico04.ej06_frigorifico.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/ar/edu/ort/p1/u4/practico04/ej06_frigorifico/src/Pais.java
+++ src/main/java/ar/edu/ort/p1/u4/practico04/ej06_frigorifico/src/Pais.java
@@ -2,5 +2,4 @@ package ar.edu.ort.p1.u4.practico04.ej06_frigorifico.src;
 
 public enum Pais {
 	ARGENTINA, CHILE, URUGUAY, BRASIL
-
 }
