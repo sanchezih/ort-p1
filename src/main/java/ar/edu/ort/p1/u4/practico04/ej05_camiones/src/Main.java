a092e0f5ea5575381a5785c7c9b3de77bdbe1f01
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2021-10-24 18:40:19 -0300

    refactor ej camiones

--- src/main/java/ar/edu/ort/p1/u4/practico04/ej05_camiones/src/Main.java
+++ src/main/java/ar/edu/ort/p1/u4/practico04/ej05_camiones/src/Main.java
@@ -1,82 +1,38 @@
 package ar.edu.ort.p1.u4.practico04.ej05_camiones.src;
 
-import ar.edu.ort.p1.u4.practico04.ej05_camiones.src.empresa.Camion;
-import ar.edu.ort.p1.u4.practico04.ej05_camiones.src.empresa.Chofer;
 import ar.edu.ort.p1.u4.practico04.ej05_camiones.src.empresa.Empresa;
 import ar.edu.ort.p1.u4.practico04.ej05_camiones.src.empresa.Viaje;
+import ar.edu.ort.p1.util.UtilArrays;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		Chofer chofer01 = new Chofer("ch01", "Angel", 10.0);
-		Chofer chofer02 = new Chofer("ch02", "Nieves", 10.0);
-		Chofer chofer03 = new Chofer("ch03", "Maria", 10.0);
-		Chofer chofer04 = new Chofer("ch04", "Orlando", 10.0);
-		Chofer chofer05 = new Chofer("ch05", "Pedro", 10.0);
-		Chofer chofer06 = new Chofer("ch06", "Vera", 10.0);
-		Chofer chofer07 = new Chofer("ch07", "Faustino", 10.0);
-		Chofer chofer08 = new Chofer("ch08", "Mara", 10.0);
-		Chofer chofer09 = new Chofer("ch09", "Jose", 10.0);
-		Chofer chofer10 = new Chofer("ch10", "Brenda", 10.0);
-		Chofer chofer11 = new Chofer("ch11", "Eva", 10.0);
-		Chofer chofer12 = new Chofer("ch12", "Rodolfo", 10.0);
-		Chofer chofer13 = new Chofer("ch13", "Ines", 10.0);
-		Chofer chofer14 = new Chofer("ch14", "Noelia", 10.0);
-		Chofer chofer15 = new Chofer("ch15", "Blanca", 10.0);
-		Chofer chofer16 = new Chofer("ch16", "Juan", 10.0);
-		Chofer chofer17 = new Chofer("ch17", "Luis", 10.0);
-		Chofer chofer18 = new Chofer("ch18", "Isabel", 10.0);
-		Chofer chofer19 = new Chofer("ch19", "Nicolas", 10.0);
-		Chofer chofer20 = new Chofer("ch20", "Julian", 10.0);
-
-		Chofer[] choferes = { chofer01, chofer02, chofer03, chofer04, chofer05, chofer06, chofer07, chofer08, chofer09,
-				chofer10, chofer11, chofer12, chofer13, chofer14, chofer15, chofer16, chofer17, chofer18, chofer19,
-				chofer20 };
-
-		Camion camion01 = new Camion(1, 150.0);
-		Camion camion02 = new Camion(2, 150.0);
-		Camion camion03 = new Camion(3, 150.0);
-		Camion camion04 = new Camion(4, 150.0);
-		Camion camion05 = new Camion(5, 150.0);
-		Camion camion06 = new Camion(6, 150.0);
-		Camion camion07 = new Camion(7, 150.0);
-		Camion camion08 = new Camion(8, 150.0);
-		Camion camion09 = new Camion(9, 150.0);
-		Camion camion10 = new Camion(10, 150.0);
-
-		Camion[] camiones = { camion01, camion02, camion03, camion04, camion05, camion06, camion07, camion08, camion09,
-				camion10 };
-
-		Empresa empresa = new Empresa(choferes, camiones);
-
+		Empresa empresa = new Empresa();
 		empresa.agregarViaje(new Viaje(1, "ch01", 1.0));
 		empresa.agregarViaje(new Viaje(2, "ch02", 1.0));
 		empresa.agregarViaje(new Viaje(2, "ch01", 1.0));
 
 		/*----------------------------------------------------------------------------*/
 		/**
-		 * Ejercicio B: La explotación del método obtenerCostosDeViajes que devuelva (no
+		 * Ejercicio B: La explotacion del metodo obtenerCostosDeViajes que devuelva (no
 		 * muestre por consola) el costo aproximado de cada viaje (honorarios +
 		 * consumo).
 		 */
-		recorrerArray(empresa.obtenerCostosDeViajes());
+
+		UtilArrays.mostrar(empresa.obtenerCostosDeViajes());
 
 		/*----------------------------------------------------------------------------*/
 		/**
-		 * Ejercicio C: La explotación del método obtenerViajesXChoferCamion que
-		 * devuelva (no muestre por consola) la cantidad de viajes que realizó cada
-		 * chofer con cada camión.
+		 * Ejercicio C: La explotacion del metodo obtenerViajesXChoferCamion que
+		 * devuelva (no muestre por consola) la cantidad de viajes que realizo cada
+		 * chofer con cada camion.
 		 */
 		dibujarMatriz(empresa.obtenerViajesXChoferCamion());
 
 	}
 
-	public static void recorrerArray(double[] elementos) {
-		for (int i = 0; i < elementos.length; i++) {
-			System.out.println(elementos[i]);
-		}
-	}
+	/*----------------------------------------------------------------------------*/
 
 	public static void dibujarMatriz(int[][] matriz) {
 		System.out.println("\nViajes X Chofer");
--- src/main/java/ar/edu/ort/p1/u4/practico04/ej05_camiones/src/empresa/Empresa.java
+++ src/main/java/ar/edu/ort/p1/u4/practico04/ej05_camiones/src/empresa/Empresa.java
@@ -10,11 +10,12 @@ public class Empresa {
 	private Chofer[] choferes = new Chofer[CANT_CHOFERES];
 	private Camion[] camiones = new Camion[CANT_CAMIONES];
 
-	public Empresa(Chofer[] choferes, Camion[] camiones) {
+	public Empresa() {
+		inicializarEmpresa();
 		this.viajes = new ArrayList<Viaje>();
-		this.choferes = choferes;
-		this.camiones = camiones;
 	}
+
+	/*----------------------------------------------------------------------------*/
 
 	public double[] obtenerCostosDeViajes() {
 		Camion elCamion;
@@ -36,6 +37,16 @@ public class Empresa {
 		return costos;
 	}
 
+	private int idxChofer(String id) {
+		int i = this.choferes.length - 1;
+		while (i >= 0 && this.choferes[i].getID() != id) {
+			i--;
+		} // Se recorre para atras para que, si no se encuentra, se devuelva -1
+		return i;
+	}
+
+	/*----------------------------------------------------------------------------*/
+
 	public int[][] obtenerViajesXChoferCamion() {
 		int[][] cantidades;
 		cantidades = new int[this.choferes.length][this.camiones.length];
@@ -47,13 +58,7 @@ public class Empresa {
 		return cantidades;
 	}
 
-	private int idxChofer(String id) {
-		int i = this.choferes.length - 1;
-		while (i >= 0 && this.choferes[i].getID() != id) {
-			i--;
-		} // Se recorre para atras para que, si no se encuentra, se devuelva -1
-		return i;
-	}
+	/*----------------------------------------------------------------------------*/
 
 	private Chofer buscarChofer(String id) {
 		Chofer elChofer = null;
@@ -66,6 +71,43 @@ public class Empresa {
 
 	/*----------------------------------------------------------------------------*/
 	/* Metodos extra */
+
+	private void inicializarEmpresa() {
+
+		// Agrego camiones
+		this.camiones[0] = new Camion(1, 150.0);
+		this.camiones[1] = new Camion(2, 150.0);
+		this.camiones[2] = new Camion(3, 150.0);
+		this.camiones[3] = new Camion(4, 150.0);
+		this.camiones[4] = new Camion(5, 150.0);
+		this.camiones[5] = new Camion(6, 150.0);
+		this.camiones[6] = new Camion(7, 150.0);
+		this.camiones[7] = new Camion(8, 150.0);
+		this.camiones[8] = new Camion(9, 150.0);
+		this.camiones[9] = new Camion(10, 150.0);
+
+		// Agrego choferes
+		this.choferes[0] = new Chofer("ch01", "Angel", 10.0);
+		this.choferes[1] = new Chofer("ch02", "Nieves", 10.0);
+		this.choferes[2] = new Chofer("ch03", "Maria", 10.0);
+		this.choferes[3] = new Chofer("ch04", "Orlando", 10.0);
+		this.choferes[4] = new Chofer("ch05", "Pedro", 10.0);
+		this.choferes[5] = new Chofer("ch06", "Vera", 10.0);
+		this.choferes[6] = new Chofer("ch07", "Faustino", 10.0);
+		this.choferes[7] = new Chofer("ch08", "Mara", 10.0);
+		this.choferes[8] = new Chofer("ch09", "Jose", 10.0);
+		this.choferes[9] = new Chofer("ch10", "Brenda", 10.0);
+		this.choferes[10] = new Chofer("ch11", "Eva", 10.0);
+		this.choferes[11] = new Chofer("ch12", "Rodolfo", 10.0);
+		this.choferes[12] = new Chofer("ch13", "Ines", 10.0);
+		this.choferes[13] = new Chofer("ch14", "Noelia", 10.0);
+		this.choferes[14] = new Chofer("ch15", "Blanca", 10.0);
+		this.choferes[15] = new Chofer("ch16", "Juan", 10.0);
+		this.choferes[16] = new Chofer("ch17", "Luis", 10.0);
+		this.choferes[17] = new Chofer("ch18", "Isabel", 10.0);
+		this.choferes[18] = new Chofer("ch19", "Nicolas", 10.0);
+		this.choferes[19] = new Chofer("ch20", "Julian", 10.0);
+	}
 
 	public void agregarViaje(Viaje viaje) {
 		this.viajes.add(viaje);
