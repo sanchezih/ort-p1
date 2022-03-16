bbe07d1836e839cc253904554ef04ae91e783ce2
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2022-03-16 10:13:59 -0300

    update enum ortdemy

--- src/main/java/ar/edu/ort/p1/unidades/u1/ejemplos/_static/_01_ejemplo_inicial/src/Main.java
+++ src/main/java/ar/edu/ort/p1/unidades/u1/ejemplos/_static/_01_ejemplo_inicial/src/Main.java
@@ -9,7 +9,7 @@ package ar.edu.ort.p1.unidades.u1.ejemplos._static._01_ejemplo_inicial.src;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Automovil a1 = new Automovil("Renault", "CIK882", "Blanco", 19000.0);
 		Automovil a2 = new Automovil("Ford", "DBN853", "Rojo", 24000.0);
 		Automovil a3 = new Automovil("Rover", "EJW901", "Gris", 32000.0);
@@ -24,6 +24,10 @@ public class Main {
 		System.out.println(a1.getPrecioPromocional());
 		System.out.println(a2.getPrecioPromocional());
 		System.out.println(a3.getPrecioPromocional());
+
+		System.out.println("\nCreo otro auto\n");
+		Automovil a4 = new Automovil("Fiat", "CXE882", "Gris", 10000.0);
+		System.out.println(a4.getPrecioPromocional());
 
 	}
 }
