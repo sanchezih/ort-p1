721711a3b2bc83ea6bb2fafeecc5743640851ff6
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2022-04-25 10:31:40 -0300

    Update Main.java

--- src/main/java/ar/edu/ort/p1/unidades/u5/ejemplos/generics/_00_ejemplos_iniciales/_01_sorteador_strings/src/Main.java
+++ src/main/java/ar/edu/ort/p1/unidades/u5/ejemplos/generics/_00_ejemplos_iniciales/_01_sorteador_strings/src/Main.java
@@ -1,6 +1,6 @@
 package ar.edu.ort.p1.unidades.u5.ejemplos.generics._00_ejemplos_iniciales._01_sorteador_strings.src;
 
-public class SorteadorDeStringsTest {
+public class Main {
 
 	public static void main(String[] args) {
 		SorteadorDeStrings sorteador = new SorteadorDeStrings();
