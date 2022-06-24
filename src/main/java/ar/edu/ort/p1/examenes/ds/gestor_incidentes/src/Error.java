61c130d8a64455905edc8b96f16e80973dfd58ac
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2022-06-24 19:09:37 -0300

    Update Error.java

--- src/main/java/ar/edu/ort/p1/examenes/ds/gestor_incidentes/src/Error.java
+++ src/main/java/ar/edu/ort/p1/examenes/ds/gestor_incidentes/src/Error.java
@@ -3,6 +3,7 @@ package ar.edu.ort.p1.examenes.ds.gestor_incidentes.src;
 import java.util.ArrayList;
 
 public abstract class Error extends Ticket {
+	
 	private String descripcion;
 	private String nombrePersonaQueLoDetecto;
 	private ArrayList<String> casosDePrueba;
