0ebcebc55bdfc57c1d4e7c8caf81769a604e80a6
IHS-WORKSTATION\ihsanch <ignacio.sanchez@comafi.com.ar>
2022-06-24 19:09:25 -0300

    Update Empleado.java

--- src/main/java/ar/edu/ort/p1/examenes/ds/gestor_incidentes/src/Empleado.java
+++ src/main/java/ar/edu/ort/p1/examenes/ds/gestor_incidentes/src/Empleado.java
@@ -1,12 +1,11 @@
 package ar.edu.ort.p1.examenes.ds.gestor_incidentes.src;
 
 public class Empleado {
+	
 	private String nombre;
 	private String apellido;
 	private String legajo;
 	private ColaDeTickets ticketsAsignados;
-	
-	
 
 	public Empleado(String nombre, String apellido, String legajo) {
 		super();
@@ -15,8 +14,6 @@ public class Empleado {
 		this.legajo = legajo;
 		this.ticketsAsignados = new ColaDeTickets();
 	}
-
-
 
 	public void agregarTicket(Ticket ticket) {
 		this.ticketsAsignados.add(ticket);
