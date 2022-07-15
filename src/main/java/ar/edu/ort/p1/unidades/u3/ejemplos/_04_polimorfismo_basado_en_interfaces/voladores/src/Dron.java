b228fec69eed4e2232d7ae6fc1a661ce6e0c425d
Ignacio Sanchez <sanchezih@gmail.com>
2022-07-15 18:34:43 -0300

    update ej interfaces

---
+++ src/main/java/ar/edu/ort/p1/unidades/u3/ejemplos/_04_polimorfismo_basado_en_interfaces/voladores/src/Dron.java
@@ -0,0 +1,17 @@
+package ar.edu.ort.p1.unidades.u3.ejemplos._04_polimorfismo_basado_en_interfaces.voladores.src;
+
+public class Dron extends Vehiculo implements Volador, Recargable {
+
+	@Override
+	public void volar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void recargar() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/ar/edu/ort/p1/unidades/u3/ejemplos/_04_polimorfismo_basado_en_interfaces/voladores/src/Volador.java
@@ -0,0 +1,5 @@
+package ar.edu.ort.p1.unidades.u3.ejemplos._04_polimorfismo_basado_en_interfaces.voladores.src;
+
+public interface Volador {
+	void volar();
+}
