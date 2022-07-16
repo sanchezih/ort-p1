08ef494d24ca475d0d049f452f707ad48f8b6868
Ignacio Sanchez <sanchezih@gmail.com>
2022-07-15 23:46:07 -0300

    Create Main.java

---
+++ src/main/java/ar/edu/ort/p1/unidades/u3/practico03/ej03_baraja_inglesa/src/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.ort.p1.unidades.u3.practico03.ej03_baraja_inglesa.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
