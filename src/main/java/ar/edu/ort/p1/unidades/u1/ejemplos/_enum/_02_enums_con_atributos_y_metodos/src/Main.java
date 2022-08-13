76bd03996c099e454ab3e18009db35efb1f0daa2
Ignacio Sanchez <sanchezih@gmail.com>
2022-08-13 13:49:13 -0300

    Update Main.java

--- src/main/java/ar/edu/ort/p1/unidades/u1/ejemplos/_enum/_02_enums_con_atributos_y_metodos/src/Main.java
+++ src/main/java/ar/edu/ort/p1/unidades/u1/ejemplos/_enum/_02_enums_con_atributos_y_metodos/src/Main.java
@@ -1,5 +1,7 @@
 package ar.edu.ort.p1.unidades.u1.ejemplos._enum._02_enums_con_atributos_y_metodos.src;
 
+import ar.edu.ort.p1.util.UtilArrays;
+
 /**
  * Ver https://www.youtube.com/watch?v=F8HSRnFGhYM
  * 
@@ -10,21 +12,21 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		/* Metodo estatico values, el cual devuelve un array del enum */
+		// Metodo estatico values, el cual devuelve un array del enum
 		Continente[] continentes = Continente.values();
 
 		/*----------------------------------------------------------------------------*/
 
-		/* Muestro los objetos */
-		for (int i = 0; i < continentes.length; i++) {
-			System.out.println(continentes[i]);
-		}
+		// Muestro los objetos
+		System.out.println("Muestro el array devuelto por el metodo values");
+		UtilArrays.mostrarGenerico(continentes);
 
 		System.out.println();
 
 		/*----------------------------------------------------------------------------*/
 
-		/* Ejecuto densidadPoblacion() por cada objeto */
+		// Ejecuto densidadPoblacion() por cada objeto
+		System.out.println("Ejecuto densidadPoblacion() por cada objeto");
 		for (int i = 0; i < continentes.length; i++) {
 			System.out.println(continentes[i].densidadPoblacion());
 		}
@@ -33,7 +35,8 @@ public class Main {
 
 		/*----------------------------------------------------------------------------*/
 
-		/* Llamo a distintos metodos de cada enum */
+		// Llamo a distintos metodos de cada enum
+		System.out.println("Llamo a distintos metodos de cada enum");
 		for (int i = 0; i < continentes.length; i++) {
 			System.out.println(
 					"El continente " + continentes[i].name() + " tiene " + continentes[i].densidadPoblacion() + " km2");
