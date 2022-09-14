88f5b4f26191131b863841216eca94fa5d6c3ac1
Ignacio Sanchez <sanchezih@gmail.com>
2022-09-13 23:00:30 -0300

    add ej licencias de conducir

---
+++ src/main/java/ar/edu/ort/p1/examenes/oop/examenes_conducir/src/Examen.java
@@ -0,0 +1,18 @@
+package ar.edu.ort.p1.examenes.oop.examenes_conducir.src;
+
+import ar.edu.ort.p1.util.pojo.Persona;
+
+public abstract class Examen implements Evaluable {
+
+	private Persona persona;
+	private String fecha;
+
+	/*----------------------------------------------------------------------------*/
+
+	public Examen(String fecha, Persona persona) {
+		super();
+		this.fecha = fecha;
+		this.persona = persona;
+	}
+
+}
