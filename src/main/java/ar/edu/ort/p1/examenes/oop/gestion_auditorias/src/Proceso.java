1e23aac672f8b708b0c43d28a0cb83f835a05e29
Ignacio Sanchez <sanchezih@gmail.com>
2022-09-20 21:31:50 -0300

    add ej auditorias

---
+++ src/main/java/ar/edu/ort/p1/examenes/oop/gestion_auditorias/src/Proceso.java
@@ -0,0 +1,40 @@
+package ar.edu.ort.p1.examenes.oop.gestion_auditorias.src;
+
+public class Proceso implements Auditable {
+
+	private final static int MAX_CANT_ACTIVIDADES = 7;
+	private String codigo;
+	private int cantActividades;
+	private boolean impactaCliente;
+	private boolean afectaPatrimonioCompania;
+
+	/*----------------------------------------------------------------------------*/
+
+	public Proceso(String codigo, int cantActividades, boolean impactaCliente, boolean afectaPatrimonioCompania) {
+		super();
+		this.codigo = codigo;
+		this.cantActividades = cantActividades;
+		this.impactaCliente = impactaCliente;
+		this.afectaPatrimonioCompania = afectaPatrimonioCompania;
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	/**
+	 * Ejercicio E: El metodo getNivelCriticidad() que permita calcular para
+	 * cualquier elemento auditable (Sistema o Proceso) su nivel de criticidad para
+	 * el negocio.
+	 */
+	@Override
+	public int getNivelCriticidad() {
+		int nivel = 0;
+
+		if (impactaCliente) {
+			nivel = 1;
+			if (cantActividades > MAX_CANT_ACTIVIDADES && afectaPatrimonioCompania) {
+				nivel = 2;
+			}
+		}
+		return nivel;
+	}
+}
