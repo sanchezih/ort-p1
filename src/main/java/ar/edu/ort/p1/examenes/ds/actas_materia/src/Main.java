1b5e00e11ac8724ce8c41c480c8ccb8d252f8bd2
Ignacio Sanchez <sanchezih@gmail.com>
2022-11-10 09:38:28 -0300

    add parcial 20221108 - actas materia

---
+++ src/main/java/ar/edu/ort/p1/examenes/ds/actas_materia/src/Main.java
@@ -0,0 +1,42 @@
+package ar.edu.ort.p1.examenes.ds.actas_materia.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Materia p1 = new Materia("Programacion 1");
+
+		// Si pertenece a un cuatrimestre anterior le corresponde el acta de FINAL
+		// PRACTICO sin importar la nota porque si curso en otro cuatrimestre y
+		// permanece inscripto en la materia es porque ya la tiene aprobada.
+		p1.agregarAlumno(new AlumnoLibre(11111111, "Noelia", 2, 8, 2));
+		p1.agregarAlumno(new AlumnoLibre(22222222, "Fernanda", 1, 8, 4));
+
+		// Si es del cuatrimestre en curso y la nota de final de cursada es mayor o
+		// igual que siete (7) y tuvo la camara encendida durante las clases, le
+		// corresponde el acta FINAL TEORICO
+		p1.agregarAlumno(new AlumnoRegular(33333333, "Nicolas", 4, 7, true));
+
+		// Si es del cuatrimestre en curso y la nota es mayor o igual que 4 pero menor
+		// a 7 o mayor o igual a 7 pero no tuvo la camara encendida durante las clases,
+		// le corresponde el acta FINAL PRACTICO.
+		p1.agregarAlumno(new AlumnoRegular(44444444, "Julian", 2, 6, true));
+		p1.agregarAlumno(new AlumnoRegular(55555555, "Erica", 2, 8, false));
+
+		// Si es del cuatrimestre en curso, en cualquier otro caso le corresponde el
+		// acta RECUPERATORIO.
+		p1.agregarAlumno(new AlumnoRegular(66666666, "Laura", 4, 2, false));
+		p1.agregarAlumno(new AlumnoRegular(77777777, "Ulises", 2, 2, true));
+
+		/*----------------------------------------------------------------------------*/
+
+		// Test ejercicio B
+		p1.asignarActas();
+		p1.mostarActas();
+		System.out.println();
+
+		// Test ejercicio C
+		p1.informarActaXCurso();
+	}
+
+}
---
+++ src/main/java/ar/edu/ort/p1/examenes/ds/actas_materia/src/Materia.java
@@ -0,0 +1,123 @@
+package ar.edu.ort.p1.examenes.ds.actas_materia.src;
+
+public class Materia {
+
+	private static final int CANT_ACTAS = CategoriaExamen.values().length;
+	private ColaDeAlumnos alumnos;
+	private Acta[] actas;
+	private String nombre;
+
+	/*----------------------------------------------------------------------------*/
+
+	public Materia(String nombre) {
+		this.alumnos = new ColaDeAlumnos();
+		this.actas = new Acta[CANT_ACTAS];
+		this.nombre = nombre;
+		inicializarActas();
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	public Acta[] getActas() {
+		return actas;
+	}
+
+	/**
+	 * Metodo que crea, dentro del array de actas, un acta por cada elemento que
+	 * exista dentro del enum CategoriaExamen
+	 */
+	private void inicializarActas() {
+		for (int i = 0; i < actas.length; i++) {
+			this.actas[i] = new Acta(CategoriaExamen.values()[i]);
+		}
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	/**
+	 * Ejercicio B: El metodo asignarActas(), el cual debe asignar los alumnos
+	 * inscriptos en una materia a sus actas correspondientes segun las reglas
+	 * anteriormente mencionadas.
+	 */
+	public void asignarActas() {
+		Alumno alumno;
+		Alumno centinela = new AlumnoRegular(-1, "", 1, 1, false);
+		this.alumnos.add(centinela);
+		alumno = this.alumnos.remove();
+
+		while (alumno != centinela) {
+			this.actas[alumno.getCategoriaExamen().ordinal()].asignar(alumno);
+			this.alumnos.add(alumno);
+			alumno = this.alumnos.remove();
+		}
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	/**
+	 * Ejercicio C: El metodo informarActaXCurso que informe (muestre por pantalla)
+	 * para cada curso cuantos alumnos hay en cada acta.
+	 * 
+	 * @return
+	 */
+	public void informarActaXCurso() {
+		Alumno alumno;
+		Alumno centinela = new AlumnoRegular(-1, "", 1, 1, false);
+		int[][] matriz = new int[CANT_ACTAS][Alumno.CANT_CURSOS];
+
+		this.alumnos.add(centinela);
+		alumno = this.alumnos.remove();
+
+		while (alumno != centinela) {
+			contarEnMatriz(matriz, alumno);
+			this.alumnos.add(alumno);
+			alumno = this.alumnos.remove();
+		}
+		mostarMatrizActaXCurso(matriz);
+	}
+
+	/**
+	 * 
+	 * @param matriz
+	 * @param alumno
+	 */
+	private void contarEnMatriz(int[][] matriz, Alumno alumno) {
+		int idxActa;
+		int idxCurso;
+		idxActa = alumno.getCategoriaExamen().ordinal();
+		idxCurso = alumno.getNroCurso() - 1;
+		matriz[idxActa][idxCurso]++;
+	}
+
+	/**
+	 * 
+	 */
+	private void mostarMatrizActaXCurso(int[][] matriz) {
+		System.out.println("Ejercicio C: Muestro para cada acta, cuantos alumnos hay de cada curso");
+		System.out.println("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\tC8\tC9\tC10");
+		for (int i = 0; i < matriz.length; i++) {
+			System.out.print(((CategoriaExamen.values()[i]).toString().substring(0, 7)) + "\t");
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	// Metodos complementarios
+
+	public void agregarAlumno(Alumno alumno) {
+		this.alumnos.add(alumno);
+	}
+
+	public void mostarActas() {
+		System.out.println("Ejercicio B: Muestro los actas de " + this.nombre);
+		for (int i = 0; i < this.actas.length; i++) {
+			this.actas[i].mostrar();
+		}
+	}
+
+}
