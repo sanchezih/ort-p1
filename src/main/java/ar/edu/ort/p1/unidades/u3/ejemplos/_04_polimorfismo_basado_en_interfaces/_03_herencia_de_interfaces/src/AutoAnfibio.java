5a7f14015f71cb330b0018a348c5253b7e562197
Ignacio Sanchez <sanchezih@gmail.com>
2023-03-29 00:52:28 -0300

    add ej u3

---
+++ src/main/java/ar/edu/ort/p1/unidades/u3/ejemplos/_04_polimorfismo_basado_en_interfaces/_03_herencia_de_interfaces/src/AutoAnfibio.java
@@ -0,0 +1,13 @@
+package ar.edu.ort.p1.unidades.u3.ejemplos._04_polimorfismo_basado_en_interfaces._03_herencia_de_interfaces.src;
+
+import java.io.Serializable;
+
+public class AutoAnfibio extends AutoBasico implements EmbarcacionMotorizada, Serializable {
+
+	@Override
+	public void launch() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
