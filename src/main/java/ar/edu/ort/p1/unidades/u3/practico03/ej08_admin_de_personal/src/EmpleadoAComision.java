6a06c3ceeb17d7385319917891b23e3109b8b6f1
Ignacio Sanchez <sanchezih@gmail.com>
2023-04-04 17:23:37 -0300

    add ej admin de personal

---
+++ src/main/java/ar/edu/ort/p1/unidades/u3/practico03/ej08_admin_de_personal/src/EmpleadoAComision.java
@@ -0,0 +1,31 @@
+package ar.edu.ort.p1.unidades.u3.practico03.ej08_admin_de_personal.src;
+
+public class EmpleadoAComision extends Empleado {
+
+	private static final double SALARIO_MINIMO = 20000;
+	private static final double MONTO_A_COBRAR_X_CLIENTE = 1000;
+	private int cantClientesCaptados;
+
+	/*----------------------------------------------------------------------------*/
+
+	public EmpleadoAComision(String dni, String nombre, String apellido, int anioIngreso, int cantClientesCaptados) {
+		super(dni, nombre, apellido, anioIngreso);
+		this.cantClientesCaptados = cantClientesCaptados;
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	public int getCantClientesCaptados() {
+		return cantClientesCaptados;
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	@Override
+	public double getSalario() {
+		double posibleSueldo = this.cantClientesCaptados * MONTO_A_COBRAR_X_CLIENTE;
+
+		// Implementacion con operador ternario
+		return posibleSueldo < SALARIO_MINIMO ? SALARIO_MINIMO : posibleSueldo;
+	}
+}
