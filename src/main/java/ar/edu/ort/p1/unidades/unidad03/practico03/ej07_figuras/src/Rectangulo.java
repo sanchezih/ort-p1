79be6255a2925348811710a98d9c923ca9d4dd65
Ignacio Sanchez <sanchezih@gmail.com>
2024-08-21 08:33:52 -0300

    add clase 2

---
+++ src/main/java/ar/edu/ort/p1/unidades/unidad03/practico03/ej07_figuras/src/Rectangulo.java
@@ -0,0 +1,28 @@
+package ar.edu.ort.p1.unidades.unidad03.practico03.ej07_figuras.src;
+
+public class Rectangulo extends Figura {
+
+	private double base;
+	private double altura;
+
+	/*----------------------------------------------------------------------------*/
+
+	public Rectangulo(String color, double base, double altura) {
+		super(color);
+		this.base = base;
+		this.altura = altura;
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	@Override
+	public double calcularArea() {
+		return this.base * this.altura;
+	}
+
+	@Override
+	public double calcularPerimetro() {
+		return 2 * this.base + 2 * this.altura;
+	}
+
+}
