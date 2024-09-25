7599c9c54fc79355f7d0027c881fc1972a538a7b
Ignacio Sanchez <sanchezih@gmail.com>
2024-09-25 09:50:25 -0300

    add clase 2024-09-24

---
+++ src/main/java/ar/edu/ort/p1/unidades/unidad04/practico04/ej04_reservas_viajes_tren/src/Estacion.java
@@ -0,0 +1,5 @@
+package ar.edu.ort.p1.unidades.unidad04.practico04.ej04_reservas_viajes_tren.src;
+
+public enum Estacion {
+	BUENOS_AIRES, LUJAN, MERCEDES, SUIPACHA, CHIVILCOY, ALBERTI, BRAGADO;
+}
