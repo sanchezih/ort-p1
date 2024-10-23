451e4609b641146c79e39413d673be64fa4a422c
Ignacio Sanchez <sanchezih@gmail.com>
2024-10-23 10:26:09 -0300

    add ej deposito

---
+++ src/main/java/ar/edu/ort/p1/examenes/_02_ds/deposito/src/ListaItemsPorNombre.java
@@ -0,0 +1,17 @@
+package ar.edu.ort.p1.examenes._02_ds.deposito.src;
+
+import ar.edu.ort.tp1.u5.tda.impl.ListaOrdenadaNodos;
+
+public class ListaItemsPorNombre extends ListaOrdenadaNodos<String, ItemPedido> {
+
+	@Override
+	public int compare(ItemPedido itemPedido1, ItemPedido itemPedido2) {
+		return itemPedido1.getNombreProducto().compareTo(itemPedido2.getNombreProducto());
+	}
+
+	@Override
+	public int compareByKey(String nombre, ItemPedido itemPedido) {
+		return nombre.compareTo(itemPedido.getNombreProducto());
+	}
+
+}
