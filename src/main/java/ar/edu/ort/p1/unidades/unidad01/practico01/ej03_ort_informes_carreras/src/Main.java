5823a22be6be4e204a9135e00a3cc18073b0ff8a
Ignacio Sanchez <sanchezih@gmail.com>
2025-03-25 10:22:43 -0300

    commit inicial 1er cuatri 2025

---
+++ src/main/java/ar/edu/ort/p1/unidades/unidad01/practico01/ej03_ort_informes_carreras/src/Main.java
@@ -0,0 +1,45 @@
+package ar.edu.ort.p1.unidades.unidad01.practico01.ej03_ort_informes_carreras.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Creo las materias
+		Materia orgEmpresarialAprobada = new Materia("Organizacion Empresarial", "2019-01", Estado.APROBADA);
+		Materia orgEmpresarialEnCurso = new Materia("Organizacion Empresarial", "2019-01", Estado.EN_CURSO);
+		Materia introInformaticaAprobada = new Materia("Introduccion a la Informatica", "2019-01", Estado.APROBADA);
+
+		// Creo los alumnos
+		Alumno fernanda = new Alumno("Fernanda", "fernanda@mail.com");
+		Alumno luis = new Alumno("Luis", "luis@mail.com");
+		Alumno facundo = new Alumno("Facundo", "facundo@mail.com");
+		Alumno melina = new Alumno("Melina", "melina@mail.com");
+
+		fernanda.agregarMateria(orgEmpresarialEnCurso);
+		luis.agregarMateria(introInformaticaAprobada);
+		facundo.agregarMateria(introInformaticaAprobada);
+		facundo.agregarMateria(orgEmpresarialAprobada);
+		luis.agregarMateria(orgEmpresarialAprobada);
+
+		// Creo las carreras
+		Carrera analistaDeSistemas = new Carrera();
+		Carrera biotecnologia = new Carrera();
+
+		// Creo alumnos a las carreras
+		analistaDeSistemas.agregarAlumno(fernanda);
+		analistaDeSistemas.agregarAlumno(luis);
+		analistaDeSistemas.agregarAlumno(facundo);
+
+		// Creo ORT
+		ORT ort = new ORT();
+		ort.agregarCarrera(analistaDeSistemas);
+		ort.agregarCarrera(biotecnologia);
+
+		/*----------------------------------------------------------------------------*/
+
+		// Test ejercicio B
+		analistaDeSistemas.mostrarInformeMaxMaterias();
+
+	}
+
+}
