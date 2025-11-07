9a325f03e46157e0add045069bfde254d26b96b0
Ignacio Sanchez <sanchezih@gmail.com>
2025-11-07 09:39:23 -0300

    add gestor de reclamos

---
+++ src/main/java/ar/edu/ort/p1/examenes/_02_ds/gestor_reclamos_afip/src/Vehiculo.java
@@ -0,0 +1,33 @@
+package ar.edu.ort.p1.examenes._02_ds.gestor_reclamos_afip.src;
+
+public abstract class Vehiculo implements Tributable {
+
+	private static final double PORCENTAJE_DE_CILINDRADA = 0.1;
+
+	private String marca;
+	private String modelo;
+	protected int cilindrada;
+
+	/*----------------------------------------------------------------------------*/
+
+	public Vehiculo(String marca, String modelo, int cilindrada) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cilindrada = cilindrada;
+	}
+
+	/*----------------------------------------------------------------------------*/
+
+	protected abstract double calcularAdicional();
+
+	/**
+	 * Ejercicio B: El metodo determinarImpuesto(), de la/s clase/s que corresponda,
+	 * que debe devolver (no mostrar por consola) el monto a abonar en impuestos.
+	 */
+	@Override
+	public double determinarImpuesto() {
+		double valorBase = this.cilindrada * PORCENTAJE_DE_CILINDRADA;
+		return valorBase + calcularAdicional();
+	}
+
+}
